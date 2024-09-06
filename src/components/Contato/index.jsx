77f990ef9deccb11faccd7ac53d2a047e5a25f59
Tiago <zokahtv@gmail.com>
2024-09-06 02:25:31 -0300

    Arrumando parte contato

--- src/components/Contato/index.jsx
+++ src/components/Contato/index.jsx
@@ -6,8 +6,12 @@ import {
     Input,
     SubmitButton,
     ContactMessage,
-    Highlight
+    Highlight,
+    TextArea
 } from "./styles"; // Importando os estilos do styles.js
+import {
+    FaUser, FaEnvelope, FaBuilding, FaPhone, FaBriefcase
+} from "react-icons/fa"; // Importando ícones
 
 const Contato = () => {
     return (
@@ -22,19 +26,27 @@ const Contato = () => {
             <FormContainer>
                 <FormTitle>Entre em Contato</FormTitle>
                 <InputGroup>
+                    <FaUser />
                     <Input type="text" placeholder="Seu Nome" />
                 </InputGroup>
                 <InputGroup>
+                    <FaEnvelope />
                     <Input type="email" placeholder="Seu E-mail" />
                 </InputGroup>
                 <InputGroup>
+                    <FaBuilding />
                     <Input type="text" placeholder="Nome da Empresa" />
                 </InputGroup>
                 <InputGroup>
+                    <FaPhone />
                     <Input type="tel" placeholder="Número de Telefone" />
                 </InputGroup>
                 <InputGroup>
+                    <FaBriefcase />
                     <Input type="text" placeholder="Segmento da Empresa" />
+                </InputGroup>
+                <InputGroup>
+                    <TextArea placeholder="Sua Mensagem" />
                 </InputGroup>
                 <SubmitButton>Enviar</SubmitButton>
             </FormContainer>
--- src/components/Contato/styles.js
+++ src/components/Contato/styles.js
@@ -2,165 +2,200 @@ import styled from "styled-components";
 import ContactBackground from "../../assets/contact-bg.jpg"; // Caminho para a imagem de fundo
 
 export const ContatoSection = styled.section`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 60px 20px;
-    height: 80vh; /* Mantém a altura como 80vh */
-    background: url(${ContactBackground}) no-repeat center center;
-    background-size: cover;
-    position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 80px 30px;
+  background: url(${ContactBackground}) no-repeat center center;
+  background-size: cover;
+  position: relative;
+  z-index: 1;
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5); /* Fundo mais sutil */
     z-index: 1;
+  }
 
-    &::before {
-        content: '';
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        background-color: rgba(0, 0, 0, 0.6);
-        z-index: 1;
-    }
+  @media (max-width: 1024px) {
+    flex-direction: column;
+    padding: 60px 20px;
+  }
 
-    @media (max-width: 1024px) {
-        flex-direction: column;
-        height: auto;
-        padding: 40px 20px;
-    }
+  @media (max-width: 768px) {
+    padding: 50px 20px;
+  }
 `;
 
 export const FormContainer = styled.div`
-    background: linear-gradient(145deg, rgba(255, 255, 255, 0.9), rgba(245, 245, 245, 0.95));
-    padding: 30px;
-    height: 500px;
-    border-radius: 10px;
-    box-shadow: 0 8px 20px rgba(0, 0, 0, 0.2);
-    max-width: 380px; /* Diminuí o tamanho máximo do formulário */
-    width: 100%;
-    z-index: 2;
-    position: relative;
-    margin-right: 200px; /* Diminuí o espaçamento da margem */
-    animation: fadeIn 1s ease-out;
+  background: rgba(255, 255, 255, 0.85); /* Fundo mais claro */
+  padding: 30px;
+  border-radius: 12px;
+  box-shadow: 0 15px 30px rgba(0, 0, 0, 0.2);
+  max-width: 400px;
+  width: 100%;
+  z-index: 2;
+  position: relative;
+  margin-right: 200px;
+  animation: fadeIn 0.7s ease-out;
+  backdrop-filter: blur(8px);
 
-    @keyframes fadeIn {
-        from {
-            opacity: 0;
-            transform: translateY(20px);
-        }
-        to {
-            opacity: 1;
-            transform: translateY(0);
-        }
-    }
-    
+  @media (max-width: 1024px) {
+    margin-right: 0;
+    margin-bottom: 20px;
+  }
 
-    @media (max-width: 1024px) {
-        margin-right: 0;
-        margin-bottom: 20px;
-    }
-
-    @media (max-width: 768px) {
-        padding: 25px;
-    }
-
-    @media (max-width: 480px) {
-        padding: 20px;
-    }
+  @media (max-width: 768px) {
+    padding: 20px;
+  }
 `;
 
 export const FormTitle = styled.h2`
-    font-size: 2rem;
-    font-weight: 700;
-    margin-bottom: 20px;
-    color: rgba(10, 25, 47, 0.85);;
-    font-family: 'Poppins', sans-serif;
-    text-align: center;
+  font-size: 1.8rem;
+  font-weight: 600;
+  margin-bottom: 20px;
+  color: rgba(10, 25, 47, 0.85);
+  font-family: 'Poppins', sans-serif;
+  text-align: center;
+  letter-spacing: 1px;
 
-    @media (max-width: 768px) {
-        font-size: 1.8rem;
-    }
+  @media (max-width: 768px) {
+    font-size: 1.6rem;
+  }
+
+  @media (max-width: 480px) {
+    font-size: 1.4rem;
+  }
 `;
 
 export const InputGroup = styled.div`
-    margin-bottom: 15px;
+  margin-bottom: 15px;
+  display: flex;
+  align-items: center;
+  position: relative;
+
+  svg {
+    position: absolute;
+    left: 10px;
+    color: rgba(10, 25, 47, 0.7);
+  }
 `;
 
 export const Input = styled.input`
-    width: 100%;
-    padding: 12px;
-    border: 2px solid transparent;
-    border-radius: 8px;
-    background-color: rgba(255, 255, 255, 0.8);
-    box-shadow: 0 3px 8px rgba(0, 0, 0, 0.1);
-    font-size: 0.9rem;
-    font-family: 'Roboto', sans-serif;
-    transition: all 0.3s ease;
-    outline: none;
+  width: 100%;
+  padding: 10px 40px;
+  border: none;
+  border-radius: 8px;
+  background-color: rgba(240, 240, 240, 0.9);
+  font-size: 16px; /* Definindo o tamanho da fonte para evitar o zoom */
+  font-family: 'Poppins', sans-serif;
+  transition: all 0.3s ease;
 
-    &:focus {
-        border-color: #DAA520;
-        background-color: rgba(255, 255, 255, 1);
-        box-shadow: 0 0 10px rgba(16, 44, 87, 0.5);
-    }
+  &:focus {
+    border-color: #DAA520;
+    background-color: rgba(255, 255, 255, 1);
+    box-shadow: 0 0 8px #DAA520;
+    outline: none;
+  }
+
+  @media (max-width: 768px) {
+    padding: 8px 35px;
+  }
 `;
 
+export const TextArea = styled.textarea`
+  width: 100%;
+  padding: 10px 40px;
+  border: none;
+  border-radius: 8px;
+  background-color: rgba(240, 240, 240, 0.9);
+  box-shadow: 0 3px 8px rgba(0, 0, 0, 0.1);
+  font-size: 16px; /* Definindo o tamanho da fonte para evitar o zoom */
+  font-family: 'Poppins', sans-serif;
+  transition: all 0.3s ease;
+  resize: none;
+  height: 100px;
+
+  &:focus {
+    border-color: #DAA520;
+    background-color: rgba(255, 255, 255, 1);
+    box-shadow: 0 0 8px #DAA520;
+    outline: none;
+  }
+
+  @media (max-width: 768px) {
+    padding: 8px 35px;
+  }
+`;
 
 
 export const SubmitButton = styled.button`
-    width: 100%;
-    padding: 12px;
-    background-color: #DAA520;
-    background: linear-gradient(145deg, #DAA520, #FFC107);
-    color: #fff;
-    font-size: 1rem;
-    font-weight: 600;
-    border: none;
-    border-radius: 8px;
-    margin-top: 16px;
-    cursor: pointer;
-    transition: all 0.3s ease;
-    box-shadow: 0 6px 12px rgba(0, 0, 0, 0.1);
+  width: 100%;
+  padding: 12px;
+  background: #DAA520;
+  color: #fff;
+  font-size: 1rem;
+  font-weight: 600;
+  border: none;
+  border-radius: 10px;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  box-shadow: 0 8px 20px rgba(0, 0, 0, 0.15);
 
-    &:hover {
-        background: linear-gradient(145deg, #FFC107, #DAA520);
-        transform: translateY(-3px);
-        box-shadow: 0 8px 18px rgba(0, 0, 0, 0.2);
-    }
+  &:hover {
+    background: #DAA443;
+    transform: translateY(-3px);
+    box-shadow: 0 10px 25px rgba(0, 0, 0, 0.2);
+  }
 
-    &:active {
-        transform: translateY(1px);
-        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-    }
+  &:active {
+    transform: translateY(1px);
+    box-shadow: 0 5px 12px rgba(0, 0, 0, 0.1);
+  }
+
+  @media (max-width: 768px) {
+    padding: 10px;
+    font-size: 0.9rem;
+  }
 `;
 
 export const ContactMessage = styled.div`
-    max-width: 450px;
-    color: #fff;
-    font-family: 'Poppins', sans-serif;
-    font-size: 1.5rem;
-    line-height: 1.6;
-    text-align: left;
-    z-index: 2;
-    position: relative;
-    margin-left: 200px;
+  max-width: 500px;
+  color: #fff;
+  font-family: 'Poppins', sans-serif;
+  font-size: 1.6rem;
+  line-height: 1.4;
+  text-align: left;
+  z-index: 2;
+  position: relative;
+  margin-left: 180px;
 
-    @media (max-width: 1024px) {
-        margin-left: 0;
-        margin-bottom: 20px;
-        text-align: center;
-    }
+  @media (max-width: 1024px) {
+    margin-left: 0;
+    margin-bottom: 20px;
+    text-align: center;
+  }
 
-    @media (max-width: 768px) {
-        font-size: 1.2rem;
-    }
+  @media (max-width: 768px) {
+    font-size: 1.2rem;
+  }
 
-    @media (max-width: 480px) {
-        font-size: 1rem;
-    }
+  @media (max-width: 480px) {
+    font-size: 1rem;
+  }
 `;
 
 export const Highlight = styled.span`
-    color: #DAA520;
-    font-weight: bold;
+  color: #DAA520;
+  font-weight: bold;
+  transition: color 0.3s ease;
+
+  &:hover {
+    color: #0096C7;
+  }
 `;
